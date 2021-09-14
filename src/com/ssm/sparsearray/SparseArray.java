3a2552cf46bcbb92c5c77446480743f4a5516e89
shaoshao <1600767556@qq.com>
2021-09-14 18:36:07 +0800

    稀疏数组测试git第一次提交

---
+++ src/com/ssm/sparsearray/SparseArray.java
@@ -0,0 +1,14 @@
+package com.ssm.sparsearray;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2021/9/14 18:30
+ */
+public class SparseArray {
+    public static void main(String[] args) {
+        //创建一个二位数组 11*11
+        //0: 表示没有去棋子  1:表示黑子  2: 表示蓝子
+    }
+
+}
