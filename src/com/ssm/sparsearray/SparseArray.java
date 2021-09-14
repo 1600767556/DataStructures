edda99ca3b3161a94153ef875dde908728b5f489
shaoshao <1600767556@qq.com>
2021-09-14 19:36:27 +0800

    二维数组---->稀疏数组

--- src/com/ssm/sparsearray/SparseArray.java
+++ src/com/ssm/sparsearray/SparseArray.java
@@ -9,6 +9,54 @@ public class SparseArray {
     public static void main(String[] args) {
         //创建一个二位数组 11*11
         //0: 表示没有去棋子  1:表示黑子  2: 表示蓝子
+        int [][] chessArr1 = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[0][2] = 1;
+        chessArr1[2][3] = 2;
+        //输出原始的二维数组
+        for (int[] row : chessArr1) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+        //二维数组 ------>  稀疏数组
+        //1.遍历原始的二维数组得到sum总数
+        int sum = 0;
+        for (int i = 0; i <chessArr1.length ; i++) {
+            for (int j = 0; j <chessArr1.length ; j++) {
+                if (chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        //2.创建对应的稀疏数组
+        int[][] sparseArr2 = new int[sum+1][3];
+        //给稀疏数组赋值
+        sparseArr2[0][0] = chessArr1.length;
+        sparseArr2[0][1] = chessArr1.length;
+        sparseArr2[0][2] = sum;
+
+        //遍历二维数组 将非0的值存放到sparseArr中
+        int  count = 0; // 用于记录第几个非0数据
+        for (int i = 0; i <chessArr1.length ; i++) {
+            for (int j = 0; j <chessArr1.length ; j++) {
+
+                if (chessArr1[i][j] != 0) {
+                    count++;
+                    sparseArr2[count][0] = i;
+                    sparseArr2[count][1] = j;
+                    sparseArr2[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+        System.out.println();
+
+        System.out.println("得到的稀疏数组为~~~~");
+        for (int i = 0; i <sparseArr2.length ; i++) {
+            System.out.printf("%d\t%d\t%d\t\n",sparseArr2[i][0],sparseArr2[i][1],sparseArr2[i][2]);
+        }
+        System.out.println();
     }
 
 }
