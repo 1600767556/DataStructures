0e177c8a4ff9941f1a90388a12d6b7533a4621c4
shaoshao <1600767556@qq.com>
2021-10-02 22:46:48 +0800

    归并排序

---
+++ src/com/ssm/sort/MergeSort.java
@@ -0,0 +1,97 @@
+package com.ssm.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2021/10/2 21:24
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        //int[] arr = {8, 4, 5, 1, 7, 3, 6, 2};
+        int[] arr = new int[8000000];
+        int [] temp = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 800000000);
+        }
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("排序前的时间是:"+simpleDateFormat.format(date));
+
+        mergeSort(arr,0,arr.length-1,temp);
+        Date date2 = new Date();
+        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("排序后的时间是:"+simpleDateFormat2.format(date2));
+
+       // int [] temp = new int[arr.length];
+        //mergeSort(arr,0,arr.length-1,temp);
+        //System.out.println("归并排序后:"+ Arrays.toString(arr));
+    }
+    //分+合的方法
+    public static void mergeSort(int[] arr, int left, int right, int[] temp){
+        if (left<right){
+            int mid = (left+right)/2;
+            //向左递归进行分解
+            mergeSort(arr,left,mid,temp);
+            //向右递归进行分解
+            mergeSort(arr,mid+1,right,temp);
+            //合并
+            merge(arr,left,mid,right,temp);
+        }
+    }
+
+    //合并的方法
+
+    /**
+     * @param arr   排序的原始数组
+     * @param left  左边有序数列的初始索引
+     * @param mid   中间索引
+     * @param right 右边索引
+     * @param temp  做中转的数组
+     */
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left; //初始化i,左边有序数列的初始索引
+        int j = mid + 1; //初始化j,右边有序数列的初始索引
+        int t = 0;//指向temp数组的当前索引
+        //(一)
+        //先把左右两边(有序)的数据按照规则填充到temp数组,直到左右两边的有序数列,有一边处理完毕
+        while (i <= mid && j <= right) {
+            //如果左边的有序数列小于等于右边的有序数列,即将有序序列的左边有序序列的当前元素,拷贝到temp数组,然后t++,i++
+            if (arr[i] < arr[j]) {
+                temp[t] = arr[i];
+                t += 1;
+                i += 1;
+            } else { //反之,将右边有序序列的当前元素,拷贝到temp数组
+                temp[t] = arr[j];
+                t += 1;
+                j += 1;
+            }
+        }
+        //(二) 把有剩余数据的一边的数据依次填充到temp
+        while (i <= mid) { //左边的有序序列还有剩余的元素,就全部填充到temp
+            temp[t] = arr[i];
+            t += 1;
+            i += 1;
+        }
+        while (j <= right) { //右边的有序序列还有剩余的元素,就全部填充到temp
+            temp[t] = arr[j];
+            t += 1;
+            j += 1;
+        }
+
+        //(三)将temp数组元素拷贝到arr  注意,并不是每次都拷贝所有
+        t = 0;
+        int tempLeft = left;
+        //System.out.println("tempLeft="+tempLeft+"right="+right);
+        while (tempLeft <= right) {
+            arr[tempLeft] = temp[t];
+            t += 1;
+            tempLeft += 1;
+        }
+    }
+
+
+}
