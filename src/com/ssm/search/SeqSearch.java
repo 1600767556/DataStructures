c57668c2796136a7e023d51c660b9d043f676932
shaoshao <1600767556@qq.com>
2021-10-03 16:24:46 +0800

    线性查找

--- src/com/ssm/search/SeqSearch.java
+++ src/com/ssm/search/SeqSearch.java
@@ -7,10 +7,10 @@ package com.ssm.search;
  */
 public class SeqSearch {
     public static void main(String[] args) {
-        int [] arr = {9,45,16,8,-4,62};
-        int index = seqSearch(arr,-4);
-        if (index!=-1){
-            System.out.println("找到了下标为:"+index);
+        int[] arr = {9, 45, 16, 8, -4, 62};
+        int index = seqSearch(arr, -4);
+        if (index != -1) {
+            System.out.println("找到了下标为:" + index);
         } else {
             System.out.println("没有找到");
         }
@@ -18,8 +18,8 @@ public class SeqSearch {
 
     public static int seqSearch(int[] arr, int value) {
         //线性查找逐一对比,发现有相同值就返回下标
-        for (int i = 0; i <arr.length ; i++) {
-            if (arr[i]==value){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
                 return i;
             }
         }
